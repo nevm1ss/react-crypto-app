2324008f76d048f0cc76456b9f1ae5ce069a774a
Artem Samokhov <103885208+nevm1ss@users.noreply.github.com>
2024-02-02 01:16:01 +0300

    ready layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import { Layout } from 'antd';
+import AppHeader from "./Components/layout/AppHeader.jsx";
+import AppSider from "./Components/layout/AppSider.jsx";
+import AppContent from "./Components/layout/AppContent.jsx";
+
 export default function App() {
-  return <h1>React Crypto Appy</h1>
+  return (
+    <Layout>
+      <AppHeader />
+      <Layout>
+        <AppSider />
+        <AppContent />
+      </Layout>
+    </Layout>
+  )
 }
